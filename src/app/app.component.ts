a61e6d6f4bee221b3695cb9b87c97fd2aba860ab
nikhil <ntnikhilesh@gmail.com>
2017-02-23 11:59:07 +0530

    RWDMQ added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    templateUrl: './app.RWDMQ.html'
+    
 })
 export class AppComponent {
     title = 'Tour of Heroes';
