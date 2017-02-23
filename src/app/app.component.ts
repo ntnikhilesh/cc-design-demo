b59b50e6be1b604504790cb53ad4ec340fe4225f
nikhil <ntnikhilesh@gmail.com>
2017-02-23 15:53:42 +0530

    button and list item added.md-card items have updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,29 @@ import { Component } from '@angular/core';
 export class AppComponent {
     title = 'Tour of Heroes';
 }
+export class ListSectionsExample {
+  folders = [
+    {
+      name: 'Photos',
+      updated: new Date('1/1/16'),
+    },
+    {
+      name: 'Recipes',
+      updated: new Date('1/17/16'),
+    },
+    {
+      name: 'Work',
+      updated: new Date('1/28/16'),
+    }
+  ];
+  notes = [
+    {
+      name: 'Vacation Itinerary',
+      updated: new Date('2/20/16'),
+    },
+    {
+      name: 'Kitchen Remodel',
+      updated: new Date('1/18/16'),
+    }
+  ];
+}
