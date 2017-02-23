5ff7ddd2b376d952177f8cc80731883247ac157a
nikhil <ntnikhilesh@gmail.com>
2017-02-23 13:20:54 +0530

    nav-sidebar and mad-cards added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-root',
-    templateUrl: './app.component.html',
+    templateUrl: './app.homecomponent.html',
     styleUrls: ['./app.component.css']
 })
 export class AppComponent {
